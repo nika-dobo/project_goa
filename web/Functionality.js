bb6d0b60c82dc59f54eaa9e76d3799adc0b6e62b
swordlegend32 <giopro1986@gmail.com>
2024-12-25 23:29:47 +0400

    Update Functionality.js

--- web/Functionality.js
+++ web/Functionality.js
@@ -1,28 +1,21 @@
-fetch("Data.json")
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok ' + response.statusText);
-    }
-    return response.json();
-  })
-  .then((data) => {
-    const Cars = data;
-    console.log(data);
-    StartGame(Cars);
-  })
-  .catch((error) => {
-    console.error('There has been a problem with your fetch operation:', error);
-  });
+fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+        const Cars = data;
+        console.log(data);
+        StartGame(Cars);
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
 
 function StartGame(Data) {
-  try {
     console.log(Data.length);
 
     function GetRandomCar() {
-      let Random = Math.floor(Math.random() * Data.length);
-      let Car = Data[Random];
-
-      return Car;
+        let Random = Math.floor(Math.random() * Data.length);
+        let Car = Data[Random];
+        return Car;
     }
 
     let isPlaying = true;
@@ -30,16 +23,48 @@ function StartGame(Data) {
 
     let car1 = GetRandomCar();
     let car2 = GetRandomCar();
-    console.log(car1);
-    console.log(car2);
-
-    while (car1 == car2) {
-      car2 = GetRandomCar();
+    while (car1 === car2) {
+        car2 = GetRandomCar();
     }
 
-    let CarNumberPrice = parseFloat(car1.price.replace("$", ""));
-    let Car2NumberPrice = parseFloat(car2.price.replace("$", ""));
-  } catch (error) {
-    console.error('Error in StartGame function:', error);
-  }
+    let CarNumberPrice = parseFloat(car1.price.replace('$', ''));
+    let Car2NumberPrice = parseFloat(car2.price.replace('$', ''));
+
+    document.getElementById('Higher').onclick = function() {
+        console.log("Higher");
+
+        if (CarNumberPrice < Car2NumberPrice) {
+            Score++;
+            car1 = car2;
+            CarNumberPrice = Car2NumberPrice;
+            car2 = GetRandomCar();
+            Car2NumberPrice = parseFloat(car2.price.replace('$', ''));
+        } else {
+            console.log("Incorrect");
+            Score = 0;
+            car1 = GetRandomCar();
+            car2 = GetRandomCar();
+            CarNumberPrice = parseFloat(car1.price.replace('$', ''));
+            Car2NumberPrice = parseFloat(car2.price.replace('$', ''));
+        }
+    };
+
+    document.getElementById('Lower').onclick = function() {
+        console.log("Lower");
+
+        if (CarNumberPrice > Car2NumberPrice) {
+            Score++;
+            car1 = car2;
+            CarNumberPrice = Car2NumberPrice;
+            car2 = GetRandomCar();
+            Car2NumberPrice = parseFloat(car2.price.replace('$', ''));
+        } else {
+            console.log("Incorrect");
+            Score = 0;
+            car1 = GetRandomCar();
+            car2 = GetRandomCar();
+            CarNumberPrice = parseFloat(car1.price.replace('$', ''));
+            Car2NumberPrice = parseFloat(car2.price.replace('$', ''));
+        }
+    };
 }
