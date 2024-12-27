46d318f465e3d0d50a8c657d612ebc42fbc994a2
swordlegend32 <giopro1986@gmail.com>
2024-12-27 18:50:59 +0400

    Finished

--- global.js
+++ global.js
@@ -2,10 +2,20 @@ console.log('global.js loaded');
 const cookie = document.cookie.split('; ').find(row => row.startsWith('username='));
 const profile = document.getElementById('profile');
 
+const usernameInfo = document.getElementById('usernameInfo');
+const emailInfo = document.getElementById('emailInfo');
+
 console.log(cookie);
+const username = cookie.split('=')[1];
+const email = document.cookie.split('; ').find(row => row.startsWith('email=')).split('=')[1];
 
 if (cookie) {
-    const username = cookie.split('=')[1];
     console.log(username);
     profile.innerText = `Welcome, ${username}`;
 }
+
+if (usernameInfo && emailInfo) {
+    usernameInfo.innerText = `${username}`;
+    emailInfo.innerText = `${email}`;
+}
+        
