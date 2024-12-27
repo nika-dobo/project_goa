9903b0a232aa91446111c9df13ab4eaa947407d0
swordlegend32 <giopro1986@gmail.com>
2024-12-27 18:20:12 +0400

    Commit

---
+++ global.js
@@ -0,0 +1,11 @@
+
+const cookie = document.cookie.split('; ').find(row => row.startsWith('username='));
+const profile = document.getElementById('profile');
+
+console.log(cookie);
+
+if (cookie) {
+    const username = cookie.split('=')[1];
+    console.log(username);
+    profile.innerText = `Welcome, ${username}`;
+}
